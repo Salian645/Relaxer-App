9f4c76c6653aa44bb9c68231b3b6d5d3f55ba845
Rakesh Salian <salianrakesh285@gmail.com>
2023-10-14 18:25:38 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.container');
+const text = document.querySelector('#text');
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+function breatheAnimation() {
+    text.innerHTML = 'Breathe In!';
+    container.className = 'container grow';
+
+    setTimeout(() => {
+        text.innerText = 'Hold';
+
+        setTimeout(() => {
+            text.innerText = 'Breathe Out!';
+            container.className = 'container shrink';
+        }, holdTime);
+    }, breatheTime);
+}
+
+// Call the `breatheAnimation` function immediately
+breatheAnimation();
+
+// Use `setInterval` to repeat the animation after the total time
+setInterval(breatheAnimation, totalTime);
